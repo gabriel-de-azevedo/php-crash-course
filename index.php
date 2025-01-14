97ed006d49e2c1cf70a2e5b4882656d731215e55
Gabriel de Azevedo <184437223+gabriel-the-azevedo@users.noreply.github.com>
2025-01-14 18:02:49 -0300

    feat: read each cookie

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
 setcookie("custom", "cookie", time() + (86400 * 30), "/");
+setcookie("custom2", "cookie2", time() + (86400 * 30), "/");
+setcookie("custom3", "cookie3", time() + (86400 * 30), "/");
+
+foreach ($_COOKIE as $key => $value) {
+    echo $key . " => " . $value . "<br>";
+}
