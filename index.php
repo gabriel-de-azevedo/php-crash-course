e348704d2266a22def16e64f9e18fb0b7e5fd8e6
Gabriel de Azevedo <184437223+gabriel-the-azevedo@users.noreply.github.com>
2025-01-14 18:25:39 -0300

    feat: print $_SERVER superglobal

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-setcookie("custom", "cookie", time() + (86400 * 30), "/");
-setcookie("custom2", "cookie2", time() + (86400 * 30), "/");
-setcookie("custom3", "cookie3", time() + (86400 * 30), "/");
-
-foreach ($_COOKIE as $key => $value) {
+foreach ($_SERVER as $key => $value) {
     echo $key . " => " . $value . "<br>";
 }
